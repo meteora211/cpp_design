fbd090b77cad9675d54f7f408d70d39dbe7c46b9
zhaorui <czhaor@gmail.com>
2022-08-31 22:32:38 +0800

    fix issues.

--- modern_cpp_design/chpt05/functor_impl.h
+++ modern_cpp_design/chpt05/functor_impl.h
@@ -5,10 +5,7 @@ template <typename R, class P>
 class FunctorImpl {
 public:
   FunctorImpl() {}
-  virtual R operator()(P param) {
-    std::cout << "operator <R,P> called" << std::endl;
-    return static_cast<R>(param);
-  }
+  virtual R operator()(P param) = 0;
   /* virtual FunctorImpl* Clone() const = 0; */
   virtual ~FunctorImpl() {}
 };
@@ -17,10 +14,7 @@ template <typename R>
 class FunctorImpl<R, NullType> {
 public:
   FunctorImpl() {}
-  virtual R operator()() {
-    std::cout << "operator <R,NullType> called" << std::endl;
-    return 0;
-  }
+  virtual R operator()() = 0;
   /* virtual FunctorImpl* Clone() const = 0; */
   virtual ~FunctorImpl() {}
 };
--- modern_cpp_design/chpt05/test.cc
+++ modern_cpp_design/chpt05/test.cc
@@ -4,17 +4,25 @@ struct TestOperator {
   void operator()(int i) {
     std::cout << "TestOperator: " << i << std::endl;
   }
-
 };
 
+void TestFunction(int i) {
+  std::cout << "TestFunction: " << i << std::endl;
+}
+
+void TestFunction() {
+  std::cout << "TestFunction void input " << std::endl;
+}
+
 int main() {
-  Functor<int, int> myFunctor1;
-  Functor<int, NullType> myFunctor2;
-
-  myFunctor1(1);
-  myFunctor2();
-
   TestOperator op;
   Functor<void, int> fun(op);
   fun(1);
+
+  typedef void(*unary)(int);
+  typedef void(*no_input)();
+  Functor<void, int> fun2(static_cast<unary>(&TestFunction));
+  Functor<void, NullType> fun3(static_cast<no_input>(&TestFunction));
+  fun2(2);
+  fun3();
 }
